4b52f629fe82b657d113653082e68880e1021b8c
ProfEibe <523386+profeibe@users.noreply.github.com>
2024-08-14 17:47:00 +0200

    Navbar Component

---
+++ src/app/app-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-navbar.component.html',
+  styleUrls: ['./app-navbar.component.css'],
+})
+export class AppNavbarComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CardModule} from "primeng/card";
+import {AppNavbarComponent} from "./app-navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardModule],
+  imports: [RouterOutlet, CardModule, AppNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
