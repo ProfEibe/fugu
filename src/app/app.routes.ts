420d9bfa93e4ae12392312096973f9f52e1a13c8
ProfEibe <523386+profeibe@users.noreply.github.com>
2024-08-21 09:27:55 +0200

    HomeRoute with Capability List

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { BadgeComponent } from './pages/badge/badge.component';
 import { ContactPickerComponent } from './pages/contact-picker/contact-picker.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
   {
     path: 'badge',
     component: BadgeComponent,
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+
+interface Capability {
+  name: string;
+  available: boolean;
+}
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CardModule, TableModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent implements OnInit {
+  capabilities: Capability[] = [];
+
+  ngOnInit() {
+    this.addCapability('Badging', 'setAppBadge' in navigator);
+    this.addCapability('Contacts', 'contacts' in navigator && 'ContactsManager' in window);
+    this.addCapability('Clipboard', 'clipboard' in navigator);
+    this.addCapability('HID', 'hid' in navigator);
+    this.addCapability('Serial', 'serial' in navigator);
+    this.addCapability('WebNFC', 'NDEFReader' in window);
+    this.addCapability('WebUSB', 'USB' in window);
+    this.addCapability('WebXR', 'xr' in navigator);
+    this.addCapability('WebBluetooth', 'bluetooth' in navigator);
+    this.addCapability('WebHID', 'hid' in navigator);
+    this.addCapability('WebSerial', 'serial' in navigator);
+    this.addCapability('WebShare', 'share' in navigator);
+  }
+
+  addCapability(name: string, available: boolean) {
+    this.capabilities.push({ name, available });
+  }
+}
