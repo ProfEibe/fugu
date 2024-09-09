29ec96e56c35914de19ffd38354695c3671899ec
ProfEibe <523386+profeibe@users.noreply.github.com>
2024-09-09 14:26:32 +0200

    simplyfied clipboard

--- src/app/pages/clipboard/clipboard.component.ts
+++ src/app/pages/clipboard/clipboard.component.ts
@@ -11,7 +11,6 @@ import { Button } from 'primeng/button';
 export class ClipboardComponent implements OnInit {
   supported = 'clipboard' in navigator;
   pastedText = '';
-  pastedImage = '';
 
   ngOnInit() {
     if (!this.supported) {
@@ -21,77 +20,20 @@ export class ClipboardComponent implements OnInit {
 
     document.addEventListener('copy', async (e) => {
       e.preventDefault();
-      this.copyFugu();
+      this.copyUrl();
     });
 
     document.addEventListener('paste', async (e) => {
       e.preventDefault();
-      this.pasteClipboardContent();
+      this.pasteText();
     });
   }
 
-  async copyText(text: string) {
-    try {
-      await navigator.clipboard.writeText(text);
-      console.log('Text copied to clipboard');
-    } catch (err) {
-      console.error('Failed to copy: ', err);
-    }
-  }
-
   async copyUrl() {
-    await this.copyText(location.href);
-  }
-
-  async copyImage(src: string) {
-    try {
-      const img = await fetch(src); // '/apple-touch-icon.png'
-      const blob = await img.blob();
-      await navigator.clipboard.write([
-        new ClipboardItem({
-          [blob.type]: blob,
-        }),
-      ]);
-      console.log('Image copied to clipboard', blob.type);
-    } catch (err) {
-      console.error('Failed to copy image: ', err);
-    }
-  }
-
-  async copyFugu() {
-    await this.copyImage('/apple-touch-icon.png');
+    await navigator.clipboard.writeText(location.href);
   }
 
   async pasteText() {
-    /*const queryOpts: any = { name: 'clipboard-read', allowWithoutGesture: false };
-    const permissionStatus = await navigator.permissions.query(queryOpts);
-    // Will be 'granted', 'denied' or 'prompt':
-    console.log(permissionStatus.state);
-
-    // Listen for changes to the permission state
-    permissionStatus.onchange = () => {
-      console.log(permissionStatus.state);
-    };*/
-
-    try {
-      this.pastedText = await navigator.clipboard.readText();
-      console.log('Pasted content: ', this.pastedText);
-    } catch (err) {
-      console.error('Failed to paste: ', err);
-    }
-  }
-
-  async pasteClipboardContent() {
-    try {
-      const clipboardItems = await navigator.clipboard.read();
-      for (const clipboardItem of clipboardItems) {
-        for (const type of clipboardItem.types) {
-          const blob = await clipboardItem.getType(type);
-          this.pastedImage = URL.createObjectURL(blob);
-        }
-      }
-    } catch (err: any) {
-      console.error(err.name, err.message);
-    }
+    this.pastedText = await navigator.clipboard.readText();
   }
 }
